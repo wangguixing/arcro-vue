46484f497a1965d5ae5616f7e45c00b4cdcb837f
wangguixing <1163260785@qq.com>
2023-03-07 14:02:58 +0800

    chore(lint-staged): lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: wangguixing 1163260785@qq.com
  * @Date: 2023-03-05 16:08:45
  * @LastEditors: wangguixing 1163260785@qq.com
- * @LastEditTime: 2023-03-06 18:09:52
+ * @LastEditTime: 2023-03-07 09:03:30
  * @FilePath: \myPages\src\App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,7 +11,7 @@ import Demo from './views/demo';
 
 export default defineComponent({
   name: 'App',
-  props: {},
+  // props: {},
   setup() {
     onMounted(() => {
       console.log(11111111);
--- src/views/demo.tsx
+++ src/views/demo.tsx
@@ -2,16 +2,15 @@
  * @Author: guixing-wang 1163260785@qq.com
  * @Date: 2023-03-05 22:46:49
  * @LastEditors: wangguixing 1163260785@qq.com
- * @LastEditTime: 2023-03-06 18:01:18
+ * @LastEditTime: 2023-03-07 09:03:38
  * @FilePath: \myPages\src\views\demo.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { onMounted } from 'vue';
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 
 export default defineComponent({
   name: 'Demo',
-  props: {},
+  // props: {},
   setup() {
     onMounted(() => {
       console.log(111);
