d651c00a854f0563ec3f57b0ee889c05ed713cde
wangguixing <1163260785@qq.com>
2023-03-30 18:19:05 +0800

    styles(reset): 样式引入问题

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
  * @Author: wangguixing 1163260785@qq.com
  * @Date: 2023-03-06 16:35:08
  * @LastEditors: wangguixing 1163260785@qq.com
- * @LastEditTime: 2023-03-06 16:38:18
+ * @LastEditTime: 2023-03-30 18:18:52
  * @FilePath: \myPages\commitlint.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,6 +28,7 @@ module.exports = {
         'chore',
         'create',
         'remove',
+        'styles',
       ],
     ],
     'type-case': [0],
